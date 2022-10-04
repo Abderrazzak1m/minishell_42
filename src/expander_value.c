d92dc34fbe18b751357cfc8dc1dbce3250ee2349
yoelhaim <youssefelhaimer8@gmail.com>
2022-10-04 15:38:48 +0100

    remove cmnt :tada:

--- src/expander_value.c
+++ src/expander_value.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   expander_value.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: amiski <amiski@student.42.fr>              +#+  +:+       +#+        */
+/*   By: yoelhaim <yoelhaim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/24 23:42:25 by yoelhaim          #+#    #+#             */
-/*   Updated: 2022/10/02 19:24:25 by amiski           ###   ########.fr       */
+/*   Updated: 2022/10/04 13:39:32 by yoelhaim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,6 @@
  
  int	expand_val_sign(char **set_value)
  {
-	 printf("%c\n", *set_value[0]);
 	 if (!check_env_value(*set_value))
 	 {
 		*set_value = ft_strdup(get_value_of_env(*set_value));
@@ -66,9 +65,9 @@ void  expand_data(t_token *token)
 		if (tmp->type == WORD)
 		{
 			if (tmp->val[0] == '~' && tmp->val[1] == '\0')
-				tmp->val = ft_strdup(get_value_of_env("HOME"));
-			if (tmp->val[0] == '~' && tmp->val[1] == '/')
-				tmp->val = ft_strdup(ft_strjoin(get_value_of_env("HOME"), "/"));
+				tmp->val = ft_strdup(getenv("HOME"));
+			if (tmp->val[0] == '~' && tmp->val[1] == '/' && tmp->val[2] == '/0')
+				tmp->val = ft_strdup(ft_strjoin(getenv("HOME"), "/"));
 			if (tmp->val[0] == '*' && tmp->val[1] == '\0')
 				ft_wildcard(&tmp->val);
 		}
