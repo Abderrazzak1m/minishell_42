62f53bae048cefe99985f97ef59f9a964b39acd8
yoelhaim <youssefelhaimer8@gmail.com>
2022-10-04 15:39:17 +0100

    fixed exported :rocket:

--- src/execution/builtin/ft_export.c
+++ src/execution/builtin/ft_export.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_export.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: amiski <amiski@student.42.fr>              +#+  +:+       +#+        */
+/*   By: yoelhaim <yoelhaim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/27 00:14:41 by yoelhaim          #+#    #+#             */
-/*   Updated: 2022/10/02 16:18:17 by amiski           ###   ########.fr       */
+/*   Updated: 2022/10/03 20:37:40 by yoelhaim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -72,21 +72,15 @@ void update_in_env(char *str, int type)
 	char *tmp;
 	char *var;
 
-	tmp = ft_split(str, '=')[1];
+	tmp = strstr(str, "=") + 1;
 	var = ft_split(str, '=')[0];
 	if (type == 1)
 	{
 		var = ft_substr(var, 0, ft_strlen(var) - 1);
-		tmp = ft_strjoin(var, ft_strjoin("=", tmp));
+		tmp = ft_strjoin(get_value_of_env(var), tmp);
 	}
-	else
-	{
-		if (!tmp)
-			return ;
-		else
-			tmp = ft_strjoin(var, ft_strjoin("=", tmp));
-	}
-	update_exit_value_env(var, tmp);
+	ft_unset(ft_split(var, ' '));
+	new_value_in_env(ft_strjoin(ft_strjoin(var,"="), tmp), 0);
 }
 
 char *get_value_of_env(char *value)
@@ -104,12 +98,40 @@ char *get_value_of_env(char *value)
 	return (NULL);
 }
 
+int check_error_export(char *str)
+{
+	int	i;
+	char *smb;
+	char *smb2;
+
+	i = 0;
+	smb= ft_strdup("@#!~_)(+*&^%01234567890");
+	smb2= ft_strdup("@#!~_)(*&^%");
+	str= *ft_split(str, '=');
+	
+	while (smb[i])
+	{
+		if (smb[i] == str[0])
+			return (ft_putendl_fd("error ", 2),0);
+		if (smb2[i] == str[ft_strlen(str) - 1])
+			return (ft_putendl_fd("error w", 2),0);
+			i++;
+	}
+	return (1);
+}
+
 void add_new_env(char **str)
 {
 	char *var;
 	int stt;
 	while (*str)
 	{
+		if (!check_error_export(*str))
+		{
+			str++;
+			continue;
+		}
+			
 		var = ft_split(*str, '=')[0];
 		if (var[ft_strlen(var) - 1] == '+')
 		{
